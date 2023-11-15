f7eee70229db031f5966edcb8e0c23140ededd8a
tzhu <thomas.zhu@lightelligence.ai>
2023-11-15 15:30:54 +0800

    exe_wl: support build, support time take display

--- exe_wl/exe_wl.py
+++ exe_wl/exe_wl.py
@@ -2,6 +2,9 @@ import sys,os,shutil
 import subprocess
 import shlex
 
+from datetime import datetime
+import time
+
 gordian_path = "/nfs/homes/tzhu/projects/pace2/gordian/"
 v8binary_path = "/nfs/homes/tzhu/projects/pace2/v8binary/"
 model_zoo_path = "/nfs/homes/tzhu/projects/pace2/model_zoo.ssd18/"
@@ -78,30 +81,22 @@ def reaadBinFile(file_path):
     except:
         assert False, "file not exist"
 
-def exeBash(cmd, disp : bool = False):
-    #print(cmd)
-    #rec = os.system(cmd)
-    #return rec
+def exeBash(cmd, timeout = None, disp : bool = False):
+    llog = []
     cmd = shlex.split(cmd)
     rc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if disp == True:
-        while rc.poll() == None:
-            l = rc.stdout.readline()
-            print(str(l, encoding='utf-8'))
-    rc.wait()
-    return rc.returncode
+    while rc.poll() == None:
+        l = str(rc.stdout.readline(), encoding='utf-8')
+        if len(l) > 1: llog.append(l[0:-1])
+        if disp == True: print(l)
+    rc.wait(timeout)
+    return (rc.returncode, llog)
 
 def md5sum(file_path_name):
     md5sum_cmd = "md5sum " + file_path_name
-    #print(md5sum_cmd)
-    cmd = shlex.split(md5sum_cmd)
-    rc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    rc.wait()
-    if rc.returncode != 0:
-        return None
-    else:
-        md5 = str(rc.stdout.readline(),encoding='utf-8')
-        return md5.split(' ')[0]
+    rc = exeBash(md5sum_cmd)
+    assert rc[0] == 0, "md5sum " + file_path_name + "\033[31mFAIL\033[0m"
+    return rc[1][0].split(' ')[0]
 
 def getGitCommit(path):
     os.chdir(path)
@@ -115,21 +110,17 @@ def getGitCommit(path):
     
 def checkOutput(golden_file_path, output_path : str = '.'):
     md5_g = md5sum(golden_file_path)
-    if None == md5_g:
-        #print("md5sum(g)", golden_file_path, "FAIL")
-        return None
     for i in range(0, 4):
         md5_o = md5sum(output_path + str.format("/output_%d.bin " % i))
-        if None == md5_o:
-            #print("md5sum(o)", output_path + str.format("/output_%d.bin " % i), "FAIL")
-            return None
-        #print("output:", md5_o)
         if md5_o != md5_g:
-            #print(md5_g)
-            #print(md5_o)
-            #print("output %d isn't same" % i)
+            print("MD5 check \033[31mFAIL\033[0m.")
             return None
     return md5_g
+
+def findFiles(path, name):
+    find_cmd = "find " + path + " -name " + name
+    rc = exeBash(find_cmd)
+    return rc[1]
 
 kWorkloads = [*dWorkloads]
 
@@ -137,25 +128,50 @@ if len(sys.argv) == 2:
     assert sys.argv[1] in kWorkloads, "arg1 not found in workload list" + str(kWorkloads)
     kWorkloads = [sys.argv[1]]
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+# print repo commits
 print("Gordian : \033[33m" + getGitCommit(gordian_path)+"\033[0m")
 print("ModelZoo: \033[33m" + getGitCommit(model_zoo_path)+"\033[0m")
 print("v8binary: \033[33m" + getGitCommit(v8binary_path)+"\033[0m")
+# try find the elf files, if not exist try rebuild.
+rc = findFiles(model_zoo_path, "*.elf")
+if len(rc) == 0: #no elf found
+    print("\033[34mNo elf found, try to build.\033[0m")
+    os.chdir(model_zoo_path)
+    rc = exeBash("make pld -j", disp = False)
+    if rc[0] != 0:
+        print("Build model_zoo failed.", rc[0])
+        sys.exit(rc[0])
+# jump to gordian folder, then run the gordian script
 os.chdir(gordian_path)
 for kwl in kWorkloads:
     wl = dWorkloads[kwl]
+    # check whether all related files exist.
+    if len(findFiles(model_zoo_path, wl["hex"])) == 0:
+        print("input+weight hex file not exist")
+        sys.exit(1)
+    if len(findFiles(model_zoo_path, wl["pe"])) == 0:
+        print("firmware image elf file not exist")
+        sys.exit(1)
+    if len(findFiles(v8binary_path, wl["output"])) == 0:
+        print("output binary file not exist")
+        sys.exit(1)
+    # execute gordian script
     gordian_cmd = "./funcsim_lin " + wl["arg"] + " hex " + model_zoo_path + wl["hex"] + " pe " + model_zoo_path + wl["pe"] + " log f dump t dumpx " + wl["dumpx"]
-    rc = exeBash(gordian_cmd, disp = False)
-    if 0 != rc:
+    start_t = datetime.now()
+    rc = exeBash(gordian_cmd, timeout = 15, disp = False)
+    if 0 != rc[0]:
         print(gordian_cmd)
         print("Gordian failed", str(rc))
         break
+    end_t = datetime.now()
+    durn = (end_t - start_t).seconds
     md5 = checkOutput(v8binary_path + wl["output"])
     if None == md5:
-        print(str("%-20s" % wl["name"]), "MD5: not same! \033[31mFAIL\033[0m")
+        print(str("%-20s" % wl["name"]), "MD5: not same! \033[31mFAIL\033[0m ")
         break
     else:
-        print(str("%-20s" % wl["name"]), "MD5:", md5, "\033[32mPASS\033[0m")
+        print(str("%-20s" % wl["name"]), "MD5:", md5, "\033[32mPASS\033[0m ", str.format("take %ds" % durn))
 print(" >>>>> done <<<<<")
-os.chdir(current_dir)
-sys.exit(rc)
+#os.chdir(current_dir)
+sys.exit(rc[0])
